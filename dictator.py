f8839b603a847dedfd1d5c38ef99aa179e58fa88
connorhsm <connorhsmith@gmail.com>
2019-12-24 21:22:21 +1100

    Added ping command

--- dictator.py
+++ dictator.py
@@ -1,8 +1,21 @@
 import discord
 
-class dictator(discord.Client):
-    async def on_ready(self):
-        print('2HOL Dictator has risen!')
+dictator = discord.Client()
 
-client = dictator()
-client.run('Token here')
+@dictator.event
+async def on_ready():
+    print('The 2HOL Dictator has risen!')
+
+@dictator.event
+async def on_message(message):
+    if message.author == dictator.user:
+        return
+
+    if message.channel.name != ('bot-topic') or ('bot-channel'):
+        return
+
+    if message.content.startswith('-ping'):
+        await message.channel.send('Pong!')
+
+
+dictator.run('Key here')
