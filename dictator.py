90459d072c490933f2e1e2852113b0ce49452e30
connorhsm <connorhsmith@gmail.com>
2019-12-26 00:51:46 +1100

    Looping status

--- dictator.py
+++ dictator.py
@@ -1,14 +1,21 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+from itertools import cycle
 
 # Ensure to create a token.txt file in the same directory as this file which contains only the token of the bot.
 
 dictator = commands.Bot(command_prefix='-')
+status = cycle(['Dictatorship', f'{dictator.command_prefix}help', 'Bullying Colin', 'Griefing Newport', 'Praising Sam'])
 
 @dictator.event
 async def on_ready():
     print('The 2HOL Dictator has risen!')
-    await dictator.change_presence(activity=discord.Game(f'Dictator | {dictator.command_prefix}help'))
+    changeStatus.start()
+    
+
+@tasks.loop(seconds=10)
+async def changeStatus():
+    await dictator.change_presence(activity=discord.Game(next(status)))
 
 @dictator.command()
 async def ping (ctx):
