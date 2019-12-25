c6bdd042df02afb1f4e8bb8a8a251f5c609e4916
connorhsm <connorhsmith@gmail.com>
2019-12-25 22:16:37 +1100

    Implemented logging

--- dictator.py
+++ dictator.py
@@ -1,6 +1,9 @@
 import discord
+import logging
 
 dictator = discord.Client()
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.WARNING)
 
 @dictator.event
 async def on_ready():
@@ -8,14 +11,15 @@ async def on_ready():
 
 @dictator.event
 async def on_message(message):
+    if message.content.startswith('-ping'):
+        logging.debug('Ive been pinged!')
+        await message.channel.send('Pong!')
+        return
+
     if message.author == dictator.user:
         return
 
-    if message.channel.name != ('bot-topic') or ('bot-channel'):
+    if message.channel.name != ('bot-channel'):
         return
-
-    if message.content.startswith('-ping'):
-        await message.channel.send('Pong!')
-
 
 dictator.run('Key here')
