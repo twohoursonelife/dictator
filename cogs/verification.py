6a06705100b60caf91b86022879cff5b2ad783f6
connorhsm <connorhsmith@gmail.com>
2019-12-31 01:46:52 +1100

    Improved verification messages

--- cogs/verification.py
+++ cogs/verification.py
@@ -48,11 +48,12 @@ class Verification(commands.Cog):
             if reaction.emoji == '✅':
                 role = get(user.guild.roles, name='Verified')
                 await user.add_roles(role, reason='User agreed to rules')
+                await user.send(content=f'Hey {user.name},\nGreat to have you here!\n**I\'ll be sending you some information soon so you can log in and play the game.**\n\nIf you have any trouble downloading, setting up or joining the game please ask for help\nin our help channel and wait patiently for someone to come to your rescue!')
 
             elif reaction.emoji == '❌':
                 invite = await reaction.message.channel.create_invite(max_uses=1, reason=f'Sent to {user.name}{user.discriminator}, user kicked after did not agree to rules')
                 await user.send(content=f'Sorry to see you go!\nIf you change your mind, you can use this link to jump back in.\n{invite}')
-                await reaction.message.guild.kick(user, reason='You did not accept the rules.')
+                await reaction.message.guild.kick(user, reason='User did not accept the rules.')
 
 
 def setup(dictator):
