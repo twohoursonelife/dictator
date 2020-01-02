432c47c8019881a12293d25b7046025ecb9417d9
connorhsm <connorhsmith@gmail.com>
2020-01-03 00:23:04 +1100

    Load prefix from config

--- src/cogs/presence.py
+++ src/cogs/presence.py
@@ -1,4 +1,5 @@
 import discord
+import yaml
 from discord.ext import commands, tasks
 from itertools import cycle
 
@@ -9,8 +10,15 @@ class Presence(commands.Cog):
 
         self.dictator = dictator
 
+        # Load config
+        with open('src/conf.yaml', 'r') as file:
+            conf = yaml.load(file, Loader=yaml.FullLoader)
+
+        # Use prefix as variable for f string compatability
+        prefix = conf['prefix']
+
         # Value should be in a config
-        self.status = cycle(['Dictatorship', '-help', 'Bullying Colin', 'Guarding Newport',
+        self.status = cycle(['Dictatorship', f'{prefix}help', 'Bullying Colin', 'Guarding Newport',
                              'Praising Sam', 'Baking the pies', 'Cheesing Uno', 'Hidei ho ho ho'])
 
     @commands.Cog.listener()
