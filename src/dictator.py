6affaa263269147cd7692ab9fcafcb46c8fb4e8a
connorhsm <connorhsmith@gmail.com>
2020-01-03 00:10:20 +1100

    Restructured flow; prefix now comes from config

--- src/dictator.py
+++ src/dictator.py
@@ -3,10 +3,26 @@ import os
 import yaml
 from discord.ext import commands
 
-# These values should be in a config file
-prefix = '-'
-dictator = commands.Bot(command_prefix=prefix)
+# Load config
+with open('src/conf.yaml', 'r') as file:
+    global conf
+    conf = yaml.load(file)
 
+# Use prefix as variable for f string compatability
+prefix = conf['prefix']
+
+# Create bot
+dictator = commands.Bot(command_prefix=conf['prefix'])
+
+# Ensure to create a token.txt file in the same directory as this file which contains only the token of the bot.
+# Reads token of bot
+with open('src/token.txt', 'r') as file:
+    dictator.run(file.readline())
+
+# Loading of all cog files in the cogs directory
+for filename in os.listdir(f'src/cogs'):
+    if filename.endswith('.py'):
+        dictator.load_extension(f'cogs.{filename[:-3]}')
 
 @dictator.event
 async def on_ready():
@@ -29,16 +45,3 @@ async def on_command_error(ctx, error):
         return
 
     print(error)
-
-# Loading of all cog files in the cogs directory
-for filename in os.listdir(f'src/cogs'):
-    if filename.endswith('.py'):
-        dictator.load_extension(f'cogs.{filename[:-3]}')
-
-# Load config
-with open('src/conf.yaml', 'r') as file:
-    conf = yaml.load(file)
-
-# Ensure to create a token.txt file in the same directory as this file which contains only the token of the bot.
-with open('src/token.txt', 'r') as file:
-    dictator.run(file.readline())
