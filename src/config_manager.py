e01e995c0dc2b5412a54cb0e8a52529c9b7d82bb
connorhsm <connorhsmith@gmail.com>
2020-02-05 13:32:57 +1100

    Created config_manager

---
+++ src/config_manager.py
@@ -0,0 +1,17 @@
+import configparser
+import os.path
+
+def check_config():
+    if os.path.isfile('src/config.ini'):
+        return('Using existing config.')
+    else:
+        config = configparser.ConfigParser()
+        config['Settings'] = {'Bot_token': 'token', 'Bot_prefix': '-', 'DB_host': 'localhost', 'DB_database': 'database', 'DB_user': 'username', 'DB_pass': 'password'}
+        with open('src/config.ini', 'w') as config_file:
+            config.write(config_file)
+        return('Config file does not exist. Creating with default values.')
+
+def read(setting):
+    config = configparser.ConfigParser()
+    config.read('src/config.ini')
+    return(config['Settings'][setting])
