0a62637000aa8c24c765161968af9e67fef29926
connorhsm <connorhsmith@gmail.com>
2020-02-05 16:18:40 +1100

    Formatting

--- src/config_manager.py
+++ src/config_manager.py
@@ -1,15 +1,18 @@
 import configparser
 import os.path
 
+
 def check_config():
     if os.path.isfile('src/config.ini'):
         return('Using existing config.')
     else:
         config = configparser.ConfigParser()
-        config['Settings'] = {'Bot_token': 'token', 'Bot_prefix': '-', 'DB_host': 'localhost', 'DB_db': 'database', 'DB_user': 'username', 'DB_pass': 'password'}
+        config['Settings'] = {'Bot_token': 'token', 'Bot_prefix': '-', 'DB_host': 'localhost',
+                              'DB_db': 'database', 'DB_user': 'username', 'DB_pass': 'password'}
         with open('src/config.ini', 'w') as config_file:
             config.write(config_file)
         return('Config file does not exist. Creating with default values.')
+
 
 def read(setting):
     config = configparser.ConfigParser()
