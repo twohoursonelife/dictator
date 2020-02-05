9a33e4f2196fb2f2bc984e71566fd5a8325fee74
connorhsm <connorhsmith@gmail.com>
2020-02-05 15:36:41 +1100

    Implemented key command for a user to be sent their login info

--- src/cogs/user.py
+++ src/cogs/user.py
@@ -1,6 +1,7 @@
 import discord
 import mysql.connector
 from discord.ext import commands
+import config_manager as config
 
 
 class User(commands.Cog):
@@ -11,12 +12,36 @@ class User(commands.Cog):
     async def createKey(parameter_list):
         pass
 
-    # Retrieve existing users information
+    # Retrieve and send a users key information to themselves
     @commands.command()
     async def key(self, ctx):
-        await ctx.author.send(f'Hey {ctx.author.mention}! Here is your login information:\n\n**Username:** USERNAME\n**Key:** KEY')
-        await ctx.send(f'{ctx.author.mention} I\'ve sent you a message with your login information.')
-        
+        try:
+            db = mysql.connector.connect(host=config.read('DB_host'), database=config.read(
+                'DB_db'), user=config.read('DB_user'), password=config.read('DB_pass'))
+            cursor = db.cursor()
+            author_id = ctx.author.id
+            cursor.execute(
+                f"SELECT email, l_key FROM `users` WHERE discord_id = {author_id}")
+            row = cursor.fetchone()
+
+            username = row[0]
+            key = row[1]
+
+            await ctx.author.send(f'Hey {ctx.author.mention}! Here is your login information:\n**Username:** {username}\n**Key:** {key}')
+            await ctx.send(f'{ctx.author.mention} I\'ve sent you a message with your login information.')
+
+            print(f'Supplied username and key to {ctx.author}')
+
+        except mysql.connector.Error as err:
+            if err.errno == mysql.connector.errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+        else:
+            db.close()
 
 
 def setup(dictator):
