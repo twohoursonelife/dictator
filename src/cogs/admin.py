c075a1b6d2b6e2eda068cc71b1f8b7580c844f6f
connorhsm <connorhsmith@gmail.com>
2020-02-11 16:46:41 +1100

    Added whois command

--- src/cogs/admin.py
+++ src/cogs/admin.py
@@ -1,5 +1,6 @@
 import discord
 import mysql.connector
+import datetime
 from discord.ext import commands
 import config_manager as config
 
@@ -130,6 +131,62 @@ class Admin(commands.Cog):
         finally:
             cursor.close()
             db.close()
+
+    @commands.command(aliases=['hois', 'who'])
+    @commands.has_role('Leader')
+    async def whois(self, ctx, *, character):
+        
+        # How many results to lookup.
+        # Due to embed length limitations, the maxium is 8.
+        history = 5
+
+        try:
+            db = mysql.connector.connect(**config.db_config())
+            cursor = db.cursor()
+            cursor.execute(f'SELECT discord_id, death_time, email FROM server_lives INNER JOIN users ON server_lives.user_id = users.id WHERE name = \'{character}\' ORDER BY death_time DESC LIMIT {history}')
+
+        except mysql.connector.Error as e:
+            print(f'\n\nMySQL Error\n{e}\n\n')
+            return
+
+        else:
+            users = cursor.fetchall()
+            if not users:
+                embed = discord.Embed(title=f'No results for the character \'{character}\'.', colour=0xffbb35)
+                await ctx.send(embed=embed)
+                return
+
+        finally:
+            cursor.close()
+            db.close()
+
+        current_time = datetime.datetime.now(tz=datetime.timezone.utc)
+        current_time = current_time.replace(microsecond=0)
+        embed = discord.Embed(title=f'Latest {history} results for the character \'{character}\':', colour=0xffbb35)
+
+        for u in users:
+            try:
+                found_user = await self.dictator.fetch_user(u[0])
+
+            except:
+                raise commands.CommandError
+                return
+            
+            else:
+                # Format death time as timezone aware
+                death_time = datetime.datetime(year=u[1].year, month=u[1].month, day=u[1].day, hour=u[1].hour, minute=u[1].minute, second=u[1].second, tzinfo=datetime.timezone.utc)
+                diff = current_time - death_time
+                diff_split = str(diff).split(':')
+                # diff_split[0] appears as '3 days, 4' where 3 = amount of days and 4 = amount of hours. I aplogise if you have to debug this.
+                diff_formatted = f'{diff_split[0]} hours, {diff_split[1]} minutes ago'
+                embed.add_field(name='Discord user:', value=f'{found_user}', inline=True)
+                embed.add_field(name='Died:', value=f'{diff_formatted}', inline=True)
+                embed.add_field(name='\u200b', value='\u200b')
+
+        if len(users) < history:
+            embed.add_field(name='\u200b', value='End of results')
+
+        await ctx.send(embed=embed)
 
 
 def setup(dictator):
