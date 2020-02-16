0d214770ba8484088239dc6942b95b2659a5d984
connorhsm <connorhsmith@gmail.com>
2020-02-16 17:16:40 +1100

    Added server cog and status command to check online player count

---
+++ src/cogs/server.py
@@ -0,0 +1,37 @@
+import discord
+import socket
+from discord.ext import commands
+
+class Server(commands.Cog):
+
+    def __init__(self, dictator):
+        self.dictator = dictator
+
+    @commands.command()
+    async def status(self, ctx):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            sock.connect(('play.twohoursonelife.com', 8005))
+            fd = sock.makefile()
+
+        except:
+            embed = discord.Embed(title='Failed to check players online')
+            await ctx.send(embed=embed)
+
+        else:
+            if 'SN' in fd.readline(128):
+                count = fd.readline(128).split('/')
+                if int(count[0]) == 1:
+                    embed = discord.Embed(title='1 player is online', colour=0xffbb35)
+                else:
+                    embed = discord.Embed(title=f'{count[0]} players are online', colour=0xffbb35)
+                
+                await ctx.send(embed=embed)
+
+        finally:
+            sock.close()
+
+
+def setup(dictator):
+    dictator.add_cog(Server(dictator))
