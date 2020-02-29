b3737aaac9283c6fc96f2fe5f543724f9b35b381
connorhsm <connorhsmith@gmail.com>
2020-03-01 02:13:53 +1100

    Purging unnecassary verification failure for user.

--- src/cogs/verification.py
+++ src/cogs/verification.py
@@ -30,13 +30,12 @@ class Verification(commands.Cog):
 
         # These messages should be in a config file
         await channel.send(file=discord.File('src/media/2HOL-600px.png'))
-        verifyMessage = await channel.send('Welcome to 2HOL!\nTo join our community, you must agree to the #rules\nThese rules can be changed at any time, but we\'ll be sure to tell you this in #notices\n\nTo tell use that you accept and will follow our rules react to this message with ✅\nIf you do not accept these rules, react to this message with ❌')
+        verifyMessage = await channel.send('Welcome to 2HOL!\nTo join our community, you must agree to the #rules\nThese rules can be changed at any time, but we\'ll be sure to tell you this in #notices\n\nTo tell us that you accept and will follow our rules react to this message with ✅')
 
         global verificationMSGID
         verificationMSGID = verifyMessage.id
 
         await verifyMessage.add_reaction('✅')
-        await verifyMessage.add_reaction('❌')
 
     # Activates when a user has reacted to verification message and processes the user.
     @commands.Cog.listener()
@@ -56,24 +55,6 @@ class Verification(commands.Cog):
                 await member.add_roles(role, reason='User successfully verified.')
                 # Finally, create a game account for the user
                 await self.dictator.get_cog('User').create_user(member)
-
-            elif emoji == '❌':
-                try:
-                    invite = await channel.create_invite(max_uses=1, reason=f'Sent to {member.name}{member.discriminator}, user kicked after did not agree to rules')
-                    await member.send(f'Sorry to see you go!\nIf you change your mind, you can use this link to jump back in.\n{invite}')
-                
-                except:
-                    print(f'Unable to message {member.name}#{member.discriminator} an invite after they failed verification.')
-                
-                try:                    
-                    await channel.guild.kick(member, reason='User did not accept the rules.')
-                
-                except discord.Forbidden as e:
-                    await member.send(f'Where do you think you\'re going, {member.mention}?\nYour soul is bound to 2HOL, remember?')
-                    print(f'{member} tried to escape, but failed miserably.')
-
-                else:
-                    print(f'{member.name}#{member.discriminator} failed verification and has been kicked from the discord.')
 
 
 def setup(dictator):
