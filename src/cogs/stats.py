ca623b80e855001e6ed6dc6fdc0b424b4b85b205
connorhsm <connorhsmith@gmail.com>
2020-03-15 22:38:45 +1100

    Online command now updates stats in general channel description and informs user.

--- src/cogs/stats.py
+++ src/cogs/stats.py
@@ -19,15 +19,26 @@ class Stats(commands.Cog):
             self.dictator.unload_extension('cogs.stats')
             return
 
-        await self.channel.edit(reason='Update player count', topic='Players in game: Loading...')
+        await self.channel.edit(reason='Update statistics', topic='Loading stats...')
 
-        self.stats.start()
+        self.stats_loop.start()
 
-    @tasks.loop(minutes=5)
-    async def stats(self):
+    @tasks.loop(minutes=15)
+    async def stats_loop(self):
+        await self.update_stats()
+
+    @commands.command(aliases=['online', 'status'], brief='Updates stats in #general channel description.', help='Updates stats in #general channel description. Replaces online/status command.')
+    async def stats(self, ctx):
+        await ctx.message.delete()
+        await self.update_stats()
+        embed = discord.Embed(title='I\'ve update the stats in the #general channel description.', description='They otherwise update every 15 minutes.', colour=0xffbb35)
+        await ctx.send(embed=embed, delete_after=5)
+
+    async def update_stats(self):
         online = await self.get_population()
 
-        await self.channel.edit(reason='Update player count', topic=f'Players in game: {online}')
+        await self.channel.edit(reason='Update statistics', topic=f'Players in game: {online}')
+        print(f'Stats updated: Players in game: {online}')
 
     async def get_population(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
