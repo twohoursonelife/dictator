9182812576a7285c64323503c2fb57a3636397d1
connorhsm <connorhsmith@gmail.com>
2020-03-24 22:30:08 +1100

    Added check if ctx channel is within guild before calling for the guild owner on error messages.

--- src/dictator.py
+++ src/dictator.py
@@ -36,8 +36,13 @@ async def on_command_error(ctx, error):
         await ctx.send(f'{ctx.author.mention} You can only run this command once at a time.')
 
     else:
-        await ctx.send(f'Uh oh... {ctx.guild.owner.mention} broke something again. Stand by.\nMake sure you\'re not blocking the bot from messaging you!')
-        print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
+        if isinstance(ctx.channel, discord.TextChannel):
+            await ctx.send(f'Uh oh... {ctx.guild.owner.mention} broke something again. Stand by.\nMake sure you\'re not blocking the bot from messaging you!')
+            print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
+        
+        else:
+            await ctx.send(f'Uh oh... Colin broke something again. Contact admin@twohoursonelife.com for assistance.')
+            print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
 
 # Loading of all cog files in the cogs directory
 for filename in os.listdir('src/cogs'):
