ae1506837fd93f31bbb4fc4518f45b1d0e892f49
connorhsm <connorhsmith@gmail.com>
2020-04-14 21:04:31 +1000

    RYFM changed to Read The 'Fricken' Manual

--- dictator/cogs/informational.py
+++ dictator/cogs/informational.py
@@ -7,9 +7,9 @@ class Informational(commands.Cog):
     def __init__(self, dictator):
         self.dictator = dictator
 
-    @commands.command(aliases=['firstinfo', 'readyourfrickenmessages', 'readyourfuckingmessages'], brief='Help information about playing for the first time.', help='Send the help information about playing for the first time, made especially for those who ignore it already sent to them from the bot.')
-    async def ryfm(self, ctx):
-        await ctx.send(f'**How to join the game for the first time.**\nRead all about it here <https://twohoursonelife.com/first-time-playing>\n\nCheck your messages from me to find your username and password.')
+    @commands.command(brief='Read the \'fricken\' manual.', help='Sends basic infoamtion about playing for the first time.')
+    async def rtfm(self, ctx):
+        await ctx.send(f'**Heres the manual to play for the first time**\n<https://twohoursonelife.com/first-time-playing>\n\nCheck your messages from me to find your username and password.')
 
 
 def setup(dictator):
