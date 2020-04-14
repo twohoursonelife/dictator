09a41ad1b9954401eae827fcf8336a1394634146
connorhsm <connorhsmith@gmail.com>
2020-04-14 21:18:17 +1000

    RTFM: Remove hard to read bold. Delete invoking command.

--- dictator/cogs/informational.py
+++ dictator/cogs/informational.py
@@ -9,7 +9,8 @@ class Informational(commands.Cog):
 
     @commands.command(brief='Read the \'fricken\' manual.', help='Sends basic infoamtion about playing for the first time.')
     async def rtfm(self, ctx):
-        await ctx.send(f'**Heres the manual to play for the first time**\n<https://twohoursonelife.com/first-time-playing>\n\nCheck your messages from me to find your username and password.')
+        await ctx.message.delete()
+        await ctx.send(f'Heres the manual to play for the first time\n<https://twohoursonelife.com/first-time-playing>\n\nCheck your messages from me to find your username and password.')
 
     @commands.Cog.listener()
     async def on_message(self, message):
