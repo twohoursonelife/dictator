0ff099b6dc9b1f2b1ffddfa554a7ba6e8a0925d3
connorhsm <connorhsmith@gmail.com>
2020-04-18 23:18:48 +1000

    Move error handling to a cog

---
+++ dictator/cogs/error_handling.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+
+
+class Error_Handling(commands.Cog):
+
+    def __init__(self, dictator):
+        self.dictator = dictator
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send(f'Invalid command. See {await self.dictator.get_prefix(ctx)}help')
+
+        elif isinstance(error, commands.MissingPermissions):
+            await ctx.send('Insufficient permissions')
+
+        elif isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f'Missing argument. See {await self.dictator.get_prefix(ctx)}help')
+
+        elif isinstance(error, commands.NoPrivateMessage):
+            await ctx.send(f'{ctx.author.mention}, you can\'t use this command in a private message. Head to the bot channel.')
+
+        elif isinstance(error, commands.MissingRole):
+            await ctx.send('You don\'t have the required role.')
+
+        elif isinstance(error, commands.CommandOnCooldown):
+            await ctx.send('{ctx.author.mention} You must wait a minute to use this command again.')
+
+        elif isinstance(error, commands.MaxConcurrencyReached):
+            await ctx.send(f'{ctx.author.mention} You can only run this command once at a time.')
+
+        else:
+            if isinstance(ctx.channel, discord.TextChannel):
+                await ctx.send(f'Uh oh... {ctx.guild.owner.mention} broke something again. Stand by.\nMake sure you\'re not blocking the bot from messaging you!')
+                print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
+            
+            else:
+                await ctx.send(f'Uh oh... Colin broke something again. Contact admin@twohoursonelife.com for assistance.')
+                print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
+
+
+def setup(dictator):
+    dictator.add_cog(Error_Handling(dictator))
--- dictator/dictator.py
+++ dictator/dictator.py
@@ -11,39 +11,6 @@ dictator = commands.Bot(command_prefix=config.read('bot_prefix'), case_insensiti
 async def on_ready():
     print('The 2HOL Dictator has risen!')
 
-# Global error handling
-@dictator.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        await ctx.send(f'Invalid command. See {await dictator.get_prefix(ctx)}help')
-
-    elif isinstance(error, commands.MissingPermissions):
-        await ctx.send('Insufficient permissions')
-
-    elif isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send(f'Missing argument. See {await dictator.get_prefix(ctx)}help')
-
-    elif isinstance(error, commands.NoPrivateMessage):
-        await ctx.send(f'{ctx.author.mention}, you can\'t use this command in a private message. Head to the bot channel.')
-
-    elif isinstance(error, commands.MissingRole):
-        await ctx.send('You don\'t have the required role.')
-
-    elif isinstance(error, commands.CommandOnCooldown):
-        await ctx.send('{ctx.author.mention} You must wait a minute to use this command again.')
-
-    elif isinstance(error, commands.MaxConcurrencyReached):
-        await ctx.send(f'{ctx.author.mention} You can only run this command once at a time.')
-
-    else:
-        if isinstance(ctx.channel, discord.TextChannel):
-            await ctx.send(f'Uh oh... {ctx.guild.owner.mention} broke something again. Stand by.\nMake sure you\'re not blocking the bot from messaging you!')
-            print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
-        
-        else:
-            await ctx.send(f'Uh oh... Colin broke something again. Contact admin@twohoursonelife.com for assistance.')
-            print(f'\n\nCOMMAND ERROR:\nAuthor: {ctx.author}\nChannel: {ctx.channel}\nCommand: {ctx.message.content}\n{error}\n\n')
-
 # Loading of all cog files in the cogs directory
 for filename in os.listdir('dictator/cogs'):
     if filename.endswith('.py'):
