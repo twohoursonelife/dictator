791aa4fe96eb46808e1044144b9f6c14c44c982d
connorhsm <connorhsmith@gmail.com>
2020-04-26 06:48:46 +1000

    Mee6 will now handle verification. dictator will create a game account for a user when they lose the unverified role.

--- dictator/cogs/verification.py
+++ dictator/cogs/verification.py
@@ -52,6 +52,15 @@ class Verification(commands.Cog):
                 # Finally, create a game account for the user
                 await self.dictator.get_cog('User').create_user(member)
 
+    @commands.Cog.listener()
+    async def on_member_update(self, before, after):
+        # We're only interested if theres been a change to a users roles
+        if before.roles != after.roles:
+            role = get(before.guild.roles, name='Unverified')
+            # User had unverfied role before, afterwards they do not have the unverified role
+            if role in before.roles and role not in after.roles:
+               # Create game account for the member
+               await self.dictator.get_cog('User').create_user(after)
 
 def setup(dictator):
     dictator.add_cog(Verification(dictator))
