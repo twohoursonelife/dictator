29af1d42c5e1d65d2ef724f62fa3a3ed33508b76
connorhsm <connorhsmith@gmail.com>
2020-05-16 02:08:19 +1000

    Added veteran player rank

--- dictator/cogs/ranks.py
+++ dictator/cogs/ranks.py
@@ -32,6 +32,30 @@ class Ranks(commands.Cog):
         await ctx.author.add_roles(role, reason='User claimed role.')
         await ctx.send(f'Congratulations, {ctx.author.mention}! You have claimed the \'Well Experienced Player\' role, for playing 50 or more hours in game! *Go take a break!*')
 
+    @commands.command(brief='Claim the \'Veteran Player\' rank.', help='If you have 375 or more hours in game, you can claim this extra special rank.')
+    async def vet(self, ctx):
+        await ctx.message.delete()
+
+        # Does the user already have the role?
+        user_has_role = discord.utils.get(ctx.author.roles, name='Veteran Player')
+        if user_has_role != None:
+            await ctx.send(f'{ctx.author.mention}, you already have this role!', delete_after=10)
+            return
+
+        with db_conn() as db:
+            db.execute(f'SELECT time_played FROM users WHERE discord_id = {ctx.author.id}')
+            time = db.fetchone()
+
+        # This user does not have 375 hours in game (Data is in minutes)
+        if int(time[0]) < 22500:
+            await ctx.send(f'{ctx.author.mention}, you do not have 375 or more hours in game. Surely just a few more to go...!', delete_after=10)
+            return
+
+        # All seems good, award the player their role!
+        role = discord.utils.get(ctx.guild.roles, name='Veteran Player')
+        await ctx.author.add_roles(role, reason='User claimed role.')
+        await ctx.send(f'Woah, {ctx.author.mention}! You have claimed the \'Veteran Player\' role, for playing 375 or more hours in game! *You\'re apart of the furniture now*')
+
 
 def setup(dictator):
     dictator.add_cog(Ranks(dictator))
