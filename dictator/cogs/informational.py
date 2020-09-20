a5164715b4fc67454c328c1360383cf43e7acec2
connorhsm <connorhsmith@gmail.com>
2020-09-21 07:40:05 +1000

    Move info command to info cog.

--- dictator/cogs/informational.py
+++ dictator/cogs/informational.py
@@ -69,6 +69,44 @@ class Informational(commands.Cog):
 
         await ctx.author.send(embed=embed)
 
+    @commands.command(brief='See info about a user.', help='Sends you a message containing information about a user.', usage='<user>')
+    @commands.guild_only()
+    async def info(self, ctx, user: discord.User):
+        await ctx.message.delete()
+
+        with db_conn() as db:
+            db.execute(f'SELECT life_count, time_played, last_activity, banned FROM users WHERE discord_id = \'{user.id}\'')
+            user_info = db.fetchone()
+
+        if not user_info:
+            embed = discord.Embed(title=f'No results for the user \'{user.mention}\'.', colour=0xffbb35)
+            await ctx.author.send(embed=embed)
+            return
+
+        # User hasn't lived a single life yet
+        if user_info[0] == None:
+            embed = discord.Embed(title=f'\'{user.name}#{user.discriminator}\' has not lived any lives yet.', colour=0xffbb35)
+            await ctx.author.send(embed=embed)
+            return
+
+        # Time formatting
+        current_time = datetime.datetime.now(tz=datetime.timezone.utc)
+        current_time = current_time.replace(microsecond=0)
+        last_active = datetime.datetime(year=user_info[2].year, month=user_info[2].month, day=user_info[2].day, hour=user_info[2].hour, minute=user_info[2].minute, second=user_info[2].second, tzinfo=datetime.timezone.utc)
+        diff = current_time - last_active
+        diff_split = str(diff).split(':')
+        # diff_split[0] appears as '3 days, 4' where 3 = amount of days and 4 = amount of hours. I aplogise if you have to debug this.
+        diff_formatted = f'{diff_split[0]} hours, {diff_split[1]} minutes ago'
+
+        # Form embed
+        embed = discord.Embed(title=f'Results for the user \'{user.name}#{user.discriminator}\':', colour=0xffbb35)
+        embed.add_field(name='Life count:', value=user_info[0])
+        embed.add_field(name='Time played:', value=f'{round(user_info[1] / 60, 1)} hours')
+        embed.add_field(name='Last death:', value=diff_formatted)
+        embed.add_field(name='Banned:', value='Yes' if user_info[3] else 'No')
+        embed.set_footer(text='Data range: August 2019 - Current')
+        await ctx.author.send(embed=embed)
+
 
 def setup(dictator):
     dictator.add_cog(Informational(dictator))
