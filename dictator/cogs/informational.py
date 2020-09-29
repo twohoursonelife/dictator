73e3edaaddb49d698b78be86a79d913550dc318d
connorhsm <connorhsmith@gmail.com>
2020-09-29 22:07:23 +1000

    Return whowas command to Admin cog

--- dictator/cogs/informational.py
+++ dictator/cogs/informational.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-import re
 from utility.db_manager import db_connection as db_conn
 import datetime
 
@@ -20,54 +19,6 @@ class Informational(commands.Cog):
         if message.content == '.key' or message.content == '.mykey':
             await message.delete()
             await message.channel.send(f'{message.author.mention} You need to use `-key`', delete_after=10)
-
-    @commands.command(aliases=['whois'], brief='Lookup who a player was in the game.', help='Lookup who a player was in the game. The player must have died. Only the last five results will be displayed. You will also be told how long ago each player died.')
-    @commands.has_any_role('Team', 'Supporters', 'Well Experienced Player', 'Veteran Player', 'What is life?')
-    async def whowas(self, ctx, *, character):
-        await ctx.message.delete()
-
-        # How many results to lookup.
-        # Due to embed length limitations, the maxium is 8.
-        history = 5
-
-        # Friendly chars only thx
-        character = re.sub(('[^a-zA-Z ]'), '', character)
-
-        with db_conn() as db:
-            db.execute('SELECT discord_id, death_time, email FROM server_lives INNER JOIN users ON server_lives.user_id = users.id WHERE name = %s ORDER BY death_time DESC LIMIT %s', (character, history))
-            users = db.fetchall()
-
-        if not users:
-            embed = discord.Embed(title=f'No results for the character \'{character}\'.', colour=0xffbb35)
-            await ctx.author.send(embed=embed)
-            return
-
-        current_time = datetime.datetime.now(tz=datetime.timezone.utc)
-        current_time = current_time.replace(microsecond=0)
-        embed = discord.Embed(title=f'Latest {history} results for the character \'{character}\':', colour=0xffbb35)
-
-        for u in users:
-            try:
-                found_user = await self.dictator.fetch_user(u[0])
-
-            except:
-                raise commands.CommandError
-
-            else:
-                # Format death time as timezone aware
-                death_time = datetime.datetime(year=u[1].year, month=u[1].month, day=u[1].day, hour=u[1].hour, minute=u[1].minute, second=u[1].second, tzinfo=datetime.timezone.utc)
-                diff = current_time - death_time
-                diff_split = str(diff).split(':')
-                # diff_split[0] appears as '3 days, 4' where 3 = amount of days and 4 = amount of hours. I aplogise if you have to debug this.
-                diff_formatted = f'{diff_split[0]} hours, {diff_split[1]} minutes ago'
-                embed.add_field(name='Discord user:', value=f'{found_user}', inline=True)
-                embed.add_field(name='Died:', value=f'{diff_formatted}', inline=True)
-                embed.add_field(name='\u200b', value='\u200b')
-
-        if len(users) < history:
-            embed.add_field(name='\u200b', value='End of results')
-
-        await ctx.author.send(embed=embed)
 
     @commands.command(brief='See info about a user.', help='Sends you a message containing information about a user.', usage='<user>')
     @commands.guild_only()
