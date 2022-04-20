09893db9c935cf1e9c18dcc9e20559e5bc5f2b9e
connorhsm <connorhsmith@gmail.com>
2022-04-21 03:44:18 +1000

    Await role assignment

--- dictator/cogs/roles.py
+++ dictator/cogs/roles.py
@@ -21,7 +21,7 @@ class Roles(commands.Cog):
             await ctx.send(f'{ctx.author.mention}, you do not have 50 or more hours in game. Keep on playin!', delete_after=10)
             return
 
-        self.assign_role(ctx, "Well Experienced", "User claimed role")
+        await self.assign_role(ctx, "Well Experienced", "User claimed role")
         await ctx.send(f'Congratulations, {ctx.author.mention}! You have claimed the \'Well Experienced\' role, for playing 50 or more hours in game! *Go take a break!*')
 
     @commands.command(brief='Claim the \'Veteran\' role.', help='If you have 375 or more hours in game, you can claim this extra special role.')
@@ -37,7 +37,7 @@ class Roles(commands.Cog):
             await ctx.send(f'{ctx.author.mention}, you do not have 375 or more hours in game. Surely just a few more to go...!', delete_after=10)
             return
             
-        self.assign_role(ctx, "Veteran", "User claimed role")
+        await self.assign_role(ctx, "Veteran", "User claimed role")
         await ctx.send(f'Woah, {ctx.author.mention}! You have claimed the \'Veteran\' role, for playing 375 or more hours in game! *You\'re apart of the furniture now*')
 
     @commands.command(brief='Claim the \'Apprentice\' role.', help='If you have 10 or more hours in game, you can claim this special role.')
@@ -53,7 +53,7 @@ class Roles(commands.Cog):
             await ctx.send(f'{ctx.author.mention}, you do not have 10 or more hours in game. Nearly!', delete_after=10)
             return
             
-        self.assign_role(ctx, "Apprentice", "User claimed role")
+        await self.assign_role(ctx, "Apprentice", "User claimed role")
         await ctx.send(f'Woohoo, {ctx.author.mention}! You have claimed the \'Apprentice\' role, for playing 10 or more hours in game! *You\'re starting to know your way around!*')
     
     def already_has_role(self, ctx, role):
