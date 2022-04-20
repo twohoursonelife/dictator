4cb5e31a120c84797acc64366eacd4591156d1a1
connorhsm <connorhsmith@gmail.com>
2022-04-21 03:23:38 +1000

    Refactor to make use of new assign_role()

--- dictator/cogs/ranks.py
+++ dictator/cogs/ranks.py
@@ -25,9 +25,7 @@ class Ranks(commands.Cog):
             await ctx.send(f'{ctx.author.mention}, you do not have 50 or more hours in game. Keep on playin!', delete_after=10)
             return
 
-        # All seems good, award the player their role!
-        role = discord.utils.get(ctx.guild.roles, name='Well Experienced')
-        await ctx.author.add_roles(role, reason='User claimed role.')
+        self.assign_role(ctx, "Well Experienced", "User claimed role")
         await ctx.send(f'Congratulations, {ctx.author.mention}! You have claimed the \'Well Experienced\' role, for playing 50 or more hours in game! *Go take a break!*')
 
     @commands.command(brief='Claim the \'Veteran\' rank.', help='If you have 375 or more hours in game, you can claim this extra special rank.')
@@ -47,9 +45,7 @@ class Ranks(commands.Cog):
             await ctx.send(f'{ctx.author.mention}, you do not have 375 or more hours in game. Surely just a few more to go...!', delete_after=10)
             return
 
-        # All seems good, award the player their role!
-        role = discord.utils.get(ctx.guild.roles, name='Veteran')
-        await ctx.author.add_roles(role, reason='User claimed role.')
+        self.assign_role(ctx, "Veteran", "User claimed role")
         await ctx.send(f'Woah, {ctx.author.mention}! You have claimed the \'Veteran\' role, for playing 375 or more hours in game! *You\'re apart of the furniture now*')
     
     def already_has_role(self, ctx, role):
