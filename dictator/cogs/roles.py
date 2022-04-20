06bc5c29850fb5c09f157a184f46e4edb7838970
connorhsm <connorhsmith@gmail.com>
2022-04-21 04:01:48 +1000

    Add constant definitions for claimable roles

--- dictator/cogs/roles.py
+++ dictator/cogs/roles.py
@@ -2,6 +2,11 @@ import discord
 from discord.ext import commands
 from utility.db_manager import db_connection as db_conn
 
+# Roles ["Role name, must match Discord", Required hours]
+ROLE_1 = ["Not Completely Lost", 10]
+ROLE_2 = ["Well Experienced", 50]
+ROLE_3 = ["Veteran", 375]
+ROLE_4 = ["What is life?", 1000]
 
 class Roles(commands.Cog):
 
