2079fc4699ec86dd4077c0feabb909bf5c3afcca
connorhsm <connorhsmith@gmail.com>
2022-08-25 01:53:07 +1000

    Small update

--- dictator/cogs/ping.py
+++ dictator/cogs/ping.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-
+import random
 
 class Ping(commands.Cog):
 
@@ -8,7 +8,10 @@ class Ping(commands.Cog):
 
     @commands.command(brief='Check the bot\'s latency.', help='Check the bot\'s latency or whether it\'s responding at all.')
     async def ping(self, ctx):
-        await ctx.send(f'Pong! That took me {round(self.dictator.latency * 1000)}ms!')
+        if random.randint(1, 100) == 1:
+            await ctx.send(f'Stop that, it hurts ;(')
+        else:
+            await ctx.send(f'Pong! That took me {round(self.dictator.latency * 1000)}ms!')
 
 
 def setup(dictator):
