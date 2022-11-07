013435b22b13805a8e69facb0810ce5d70a22640
Connor Smith <connorhsmith@gmail.com>
2022-11-08 01:08:38 +1100

    Final conversion to new config manager

--- dictator/utility/db_manager.py
+++ dictator/utility/db_manager.py
@@ -1,11 +1,17 @@
-from utility.config_manager import db_config
 import mysql.connector
+from constants import DB_HOST, DB_DATABASE, DB_USER, DB_PASSWORD
 
 
 class db_connection:
     
     def __init__(self):
-        self.db = mysql.connector.connect(**db_config(), autocommit=True)
+        self.db = mysql.connector.connect(
+            host = DB_HOST,
+            database = DB_DATABASE,
+            user = DB_USER,
+            password = DB_PASSWORD,
+            autocommit = True)
+
         self.cursor = self.db.cursor()
 
     def __enter__(self):
