504110b09540665a3ab9e2d55efbcb363802e95f
Connor Smith <connorhsmith@gmail.com>
2022-12-18 16:59:34 +1100

    Update user account creation not to happen before user passes guild rules agreement

--- dictator/cogs/user.py
+++ dictator/cogs/user.py
@@ -11,10 +11,13 @@ class User(commands.Cog):
     def __init__(self, dictator):
         self.dictator = dictator
 
-    # Create a game account for a user as soon as they join the Discord guild
+    # Trigger account creation after member passes guild rules screening
     @commands.Cog.listener()
-    async def on_member_join(self, member):
-        await self.create_user(member)
+    async def on_member_update(self, before: discord.Member, after: discord.Member) -> None:
+        if not before.pending and after.pending:
+            return
+        
+        await self.create_user(after)
 
     # Retrieve and send a users login information to themselves
     @commands.command(aliases=['mykey'], brief='Retireve your login information for the game.', help='Retireve your login information for the game. If you don\'t have an account, one will be created for you.')
