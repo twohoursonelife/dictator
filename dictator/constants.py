d15c2752a4f2983d6e9a65fc7ea1f2eb4df99a91
Connor Smith <connorhsmith@gmail.com>
2023-01-24 01:04:15 +1100

    Generating data time period to analyse based on months input
    And other tidying of the OpenCollective Class

--- dictator/constants.py
+++ dictator/constants.py
@@ -17,5 +17,5 @@ DEBUG_CHANNEL_ID = int(os.environ.get("DEBUG_CHANNEL_ID", 973930931456987206))
 STATS_CHANNEL_ID = int(os.environ.get("STATS_CHANNEL_ID", 744031336448393328))
 
 OC_GRAPHQL_ENDPOINT = os.environ.get("OC_GRAPHQL_ENDPOINT", "https://api.opencollective.com/graphql/v2")
-OC_GRAPHQL_KEY = os.environ["OC_GRAPHQL_KEY"]
 OC_ANALYSIS_PERIOD_MONTHS = int(os.environ.get("OC_ANALYSIS_PERIOD_MONTHS", 6))
+OC_GRAPHQL_KEY = os.environ["OC_GRAPHQL_KEY"]
--- dictator/helpers/open_collective.py
+++ dictator/helpers/open_collective.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, time
 from dateutil.relativedelta import relativedelta
 from constants import OC_GRAPHQL_ENDPOINT, OC_GRAPHQL_KEY, OC_ANALYSIS_PERIOD_MONTHS
 import requests
@@ -6,14 +6,33 @@ import json
 
 class OpenCollective:
     """"""
+    
+    def __get_data_time_period(period_months: int) -> tuple[datetime]:
+        
+        now = datetime.now()
+        
+        start_month = now - relativedelta(months=period_months, days=now.day - 1)
+        first_second_start_month = datetime.combine(start_month, time.min)
+        
+        first_day_this_month = now.replace(day=1)
+        last_day_last_month = first_day_this_month - relativedelta(days=1)
+        last_second_last_month = datetime.combine(last_day_last_month, time.max)
+        
+        return (first_second_start_month, last_second_last_month)
 
 
-    def __get_open_collective_data(period_months: int) -> dict:
-        # TODO
-        # Adjustable date
+    def __get_open_collective_data(period_range: tuple[datetime]) -> dict:
+        
+        dateFrom = period_range[0].isoformat() + "Z"
+        dateTo = period_range[1].isoformat() + "Z"
+        
+        varibles = {
+            "dateFrom": dateFrom,
+            "dateTo": dateTo
+        }
         
         query = """
-        query {
+        query($dateFrom: DateTime, $dateTo: DateTime) {
             collective(slug: "twohoursonelife") {
                 stats {
                     balance {
@@ -23,8 +42,8 @@ class OpenCollective:
             }
             transactions(
                 account: [{ slug: "twohoursonelife" }]
-                dateFrom: "2022-07-01T00:00:00Z"
-                dateTo: "2022-12-31T23:59:59Z"
+                dateFrom: $dateFrom
+                dateTo: $dateTo
             )   {
                 nodes {
                     kind
@@ -37,20 +56,23 @@ class OpenCollective:
             }
         """
         
+        payload = {
+            "query": query,
+            "varibles": varibles
+        }
+        
         headers = {
             'Content-Type': 'application/json',
             'Api-key': OC_GRAPHQL_KEY
         }
         
-        response = requests.post(OC_GRAPHQL_ENDPOINT,
-                                 json={'query': query}, headers=headers)
+        response = requests.post(OC_GRAPHQL_ENDPOINT, json=payload, headers=headers)
         
-        if response.status_code != 200:
-            raise Exception(f"Open Collective GraphQL query failed, returning {response.status_code} {query}")
+        response.raise_for_status()
         
         return json.loads(response.content)
     
-    def __get_average_cash_flow(period_months: int, data: dict) -> dict:
+    def __get_average_cash_flow(period_months: int, data: dict) -> dict[str: int]:
         incoming = 0
         outgoing = 0
         for t in data["data"]["transactions"]["nodes"]:
@@ -69,22 +91,23 @@ class OpenCollective:
     def __get_balance(data: dict) -> int:
         return data["data"]["collective"]["stats"]["balance"]["value"]
     
-    def __forecast_negative_cash_date(start_balance: int, cash_flow: int) -> int:
+    def __forecast_negative_cash_date(start_balance: int, cash_flow: int) -> datetime.date:
         months = 0
         while start_balance > 0:
             start_balance += cash_flow
             months += 1
 
-        return (date.today() + relativedelta(months=+months))#.strftime("%B")
+        return date.today() + relativedelta(months=months, day=1)
     
     @classmethod
-    def forecast(self):
-        data = self.__get_open_collective_data(OC_ANALYSIS_PERIOD_MONTHS)
+    def forecast(self) -> dict[str: datetime]:
+        time_period = self.__get_data_time_period(OC_ANALYSIS_PERIOD_MONTHS)
+        data = self.__get_open_collective_data(time_period)
         cash_flow = self.__get_average_cash_flow(OC_ANALYSIS_PERIOD_MONTHS, data)
         balance = self.__get_balance(data)
         
         return {
-            "no_income": self.__forecast_negative_cash_date(balance, cash_flow["outgoing"]),
-            "forecast_income": self.__forecast_negative_cash_date(balance, sum(cash_flow.values()))
+            "forecast_no_income": self.__forecast_negative_cash_date(balance, cash_flow["outgoing"]),
+            "forecast_continued_income": self.__forecast_negative_cash_date(balance, sum(cash_flow.values()))
         }
     
