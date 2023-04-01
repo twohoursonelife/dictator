34cf1216b3c712d31f8b07c99ab661f69db1ec49
Connor Smith <connorhsmith@gmail.com>
2023-04-01 23:32:20 +1100

    Reorganise required environment variables

--- dictator/constants.py
+++ dictator/constants.py
@@ -5,14 +5,16 @@ load_dotenv()
 
 # Required environment variables
 BOT_TOKEN = os.environ["BOT_TOKEN"]
-DB_HOST = os.environ["DB_HOST"]
-DB_DATABASE = os.environ["DB_DB"]
-DB_USER = os.environ["DB_USER"]
 DB_PASSWORD = os.environ["DB_PASS"]
 OC_GRAPHQL_KEY = os.environ["OC_GRAPHQL_KEY"]
 
 # Optional environment variables, setting defaults
 BOT_PREFIX = os.environ.get("BOT_PREFIX", "-")
+
+DB_HOST = os.environ.get("DB_HOST", "db.twohoursonelife.com")
+DB_DATABASE = os.environ.get("DB_DB", "PROD_2HOL")
+DB_USER = os.environ.get("DB_USER", "dictator")
+
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 620375198251745282))
 DEBUG_CHANNEL_ID = int(os.environ.get("DEBUG_CHANNEL_ID", 973930931456987206))
 STATS_CHANNEL_ID = int(os.environ.get("STATS_CHANNEL_ID", 744031336448393328))
