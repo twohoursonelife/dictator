129a6715d4cc44d304e7825a708d1bebeeb03357
Connor Smith <connorhsmith@gmail.com>
2023-05-07 22:42:28 +1000

    Update Roles app commands to defer interaction response due to database load time

--- dictator/cogs/roles.py
+++ dictator/cogs/roles.py
@@ -17,62 +17,66 @@ class Roles(commands.Cog):
     @app_commands.command()
     async def ncl(self, interaction: discord.Interaction) -> None:
         """Claims the NCL role if you have 10 or more hours in game. Grants you more access to the server."""
+        await interaction.response.defer(ephemeral=True)
 
         if self.already_has_role(interaction, ROLE_1["name"]):
-            await interaction.response.send_message(f'{interaction.user.mention}, you already have this role!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you already have this role!', ephemeral=True)
             return
 
         if self.playtime_less_than(interaction.user.id, ROLE_1["hours"] * 60):
-            await interaction.response.send_message(f'{interaction.user.mention}, you do not have {ROLE_1["hours"]} or more hours in game. Nearly!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you do not have {ROLE_1["hours"]} or more hours in game. Nearly!', ephemeral=True)
             return
             
         await self.assign_role(interaction, ROLE_1["name"], "User claimed role")
-        await interaction.response.send_message(f'Woohoo, {interaction.user.mention}! You have claimed the \'{ROLE_1["name"]}\' role, for playing {ROLE_1["hours"]} or more hours in game! *You\'re starting to know your way around!*')
+        await interaction.followup.send(f'Woohoo, {interaction.user.mention}! You have claimed the \'{ROLE_1["name"]}\' role, for playing {ROLE_1["hours"]} or more hours in game! *You\'re starting to know your way around!*')
 
     @app_commands.command()
     async def exp(self, interaction: discord.Interaction) -> None:
         """Claims the EXP role if you have 50 or more hours in game. Grants you more access to the server."""
+        await interaction.response.defer(ephemeral=True)
 
         if self.already_has_role(interaction, ROLE_2["name"]):
-            await interaction.response.send_message(f'{interaction.user.mention}, you already have this role!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you already have this role!', ephemeral=True)
             return
 
         if self.playtime_less_than(interaction.user.id, ROLE_2["hours"] * 60):
-            await interaction.response.send_message(f'{interaction.user.mention}, you do not have {ROLE_2["hours"]} or more hours in game. Keep on playin!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you do not have {ROLE_2["hours"]} or more hours in game. Keep on playin!', ephemeral=True)
             return
 
         await self.assign_role(interaction, ROLE_2["name"], "User claimed role")
-        await interaction.response.send_message(f'Congratulations, {interaction.user.mention}! You have claimed the \'{ROLE_2["name"]}\' role, for playing {ROLE_2["hours"]} or more hours in game! *Go take a break!*')
+        await interaction.followup.send(f'Congratulations, {interaction.user.mention}! You have claimed the \'{ROLE_2["name"]}\' role, for playing {ROLE_2["hours"]} or more hours in game! *Go take a break!*')
 
     @app_commands.command()
     async def vet(self, interaction: discord.Interaction) -> None:
         """Claims the VET role if you have 375 or more hours in game. Grants you more access to the server."""
+        await interaction.response.defer(ephemeral=True)
 
         if self.already_has_role(interaction, ROLE_3["name"]):
-            await interaction.response.send_message(f'{interaction.user.mention}, you already have this role!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you already have this role!', ephemeral=True)
             return
 
         if self.playtime_less_than(interaction.user.id, ROLE_3["hours"] * 60):
-            await interaction.response.send_message(f'{interaction.user.mention}, you do not have {ROLE_3["hours"]} or more hours in game. Surely just a few more to go...!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you do not have {ROLE_3["hours"]} or more hours in game. Surely just a few more to go...!', ephemeral=True)
             return
             
         await self.assign_role(interaction, ROLE_3["name"], "User claimed role")
-        await interaction.response.send_message(f'Woah, {interaction.user.mention}! You have claimed the \'{ROLE_3["name"]}\' role, for playing {ROLE_3["hours"]} or more hours in game! *You\'re apart of the furniture now*')
+        await interaction.followup.send(f'Woah, {interaction.user.mention}! You have claimed the \'{ROLE_3["name"]}\' role, for playing {ROLE_3["hours"]} or more hours in game! *You\'re apart of the furniture now*')
     
     @app_commands.command()
     async def wil(self, interaction: discord.Interaction) -> None:
         """Claims the WIL role if you have 1,000 or more hours in game. Grants you more access to the server."""
+        await interaction.response.defer(ephemeral=True)
 
         if self.already_has_role(interaction, ROLE_4["name"]):
-            await interaction.response.send_message(f'{interaction.user.mention}, you already have this role! Doh!', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you already have this role! Doh!', ephemeral=True)
             return
 
         if self.playtime_less_than(interaction.user.id, ROLE_4["hours"] * 60):
-            await interaction.response.send_message(f'{interaction.user.mention}, you do not have {ROLE_4["hours"]} or more hours in game. Just around the corner, *right?*', ephemeral=True)
+            await interaction.followup.send(f'{interaction.user.mention}, you do not have {ROLE_4["hours"]} or more hours in game. Just around the corner, *right?*', ephemeral=True)
             return
             
         await self.assign_role(interaction, ROLE_4["name"], "User claimed role")
-        await interaction.response.send_message(f'Woah, {interaction.user.mention}! You have claimed the \'{ROLE_4["name"]}\' role, for playing {ROLE_4["hours"]} or more hours in game! *I suppose you can go now*')
+        await interaction.followup.send(f'Woah, {interaction.user.mention}! You have claimed the \'{ROLE_4["name"]}\' role, for playing {ROLE_4["hours"]} or more hours in game! *I suppose you can go now*')
 
     def already_has_role(self, interaction: discord.Interaction, role) -> bool:
         role_object = discord.utils.get(interaction.user.roles, name=role)
