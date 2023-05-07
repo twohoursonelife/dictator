f0e024f96dd219fdf31283719147e3f5de4a170c
Connor Smith <connorhsmith@gmail.com>
2023-05-07 22:56:32 +1000

    Convert System Cog to use app commands
    With minor logic changes and use of ephemeral

--- dictator/cogs/system.py
+++ dictator/cogs/system.py
@@ -1,5 +1,9 @@
+import discord
+from discord import app_commands
+
 from discord.ext import commands
 from discord.ext.commands import Context
+
 import random
 
 class System(commands.Cog):
@@ -7,21 +11,25 @@ class System(commands.Cog):
     def __init__(self, dictator: commands.Bot) -> None:
         self.dictator = dictator
 
-    @commands.command(brief='Check the bot\'s latency.', help='Check the bot\'s latency or whether it\'s responding at all.')
-    async def ping(self, ctx: Context) -> None:
+    @app_commands.command()
+    async def ping(self, interaction: discord.Interaction) -> None:
+        """Check the latency between Discord and Dictator."""
+        
         if random.randint(1, 100) == 1:
-            await ctx.send(f'Stop that, it hurts ;(')
-        else:
-            await ctx.send(f'Pong! That took me {round(self.dictator.latency * 1000)}ms!')
-            
-    @commands.command(brief='Replies with Dictators\'s current version.', help='Check the bot\'s version.')
-    async def version(self, ctx: Context) -> None:
+            return await interaction.response.send_message(f"Stop that, it hurts ;(", ephemeral=True)
+        
+        await interaction.response.send_message(f"Pong! That took me {round(self.dictator.latency * 1000)}ms to get a response from Discord!", ephemeral=True)
+        
+    @app_commands.command()    
+    async def version(self, interaction: discord.Interaction) -> None:
+        """Check the current version of Dictator."""
+        
         with open('version.txt', 'r') as file:
-            await ctx.send(file.read())
+            await interaction.response.send_message(file.read(), ephemeral=True)
             
-    @commands.command(brief='Sync Dictators app commands globally.')
     @commands.guild_only()
     @commands.is_owner()
+    @commands.command(brief='Sync Dictators app commands globally.')
     async def sync(self, ctx: Context) -> None:
         synced = await ctx.bot.tree.sync()
         await ctx.send(f'Synced `{len(synced)}` commands globally.')
