e52c8d93af014a8ad6362fc342fa5b2624eef7d0
Connor Smith <connorhsmith@gmail.com>
2023-08-06 01:06:23 +1000

    Update presence messages

--- dictator/cogs/presence.py
+++ dictator/cogs/presence.py
@@ -8,17 +8,22 @@ class Presence(commands.Cog):
         self.dictator = dictator
 
         self.status = cycle(
+            # 1: Playing, 2: Listening to, 3: Watching, 5: Competing in
             [
-                "Dictatorship",
-                "-help",
-                "Bullying Colin",
-                "Guarding Newport",
-                "Praising Sam",
-                "Baking the pies",
-                "Cheesing Uno",
-                "Hidei ho ho ho",
-                "Celebrating Four Years",
-                "Munching Berries",
+                (3, "Kripts"),
+                (2, "Twisted"),
+                (1, "with Uno"),
+                (3, "Sammoh shine"),
+                (3, "over Newport"),
+                (5, "Dictatorship"),
+                (5, "cheese eating"),
+                (5, "trolling Moon"),
+                (2, "CartoonJessie"),
+                (1, "for Five Years"),
+                (2, "Hidei ho ho ho"),
+                (5, "baking the pies"),
+                (5, "munching berries"),
+                (5, "bullying Tahtekcub"),
             ]
         )
 
@@ -26,9 +31,11 @@ class Presence(commands.Cog):
     async def on_ready(self):
         self.change_status.start()
 
-    @tasks.loop(seconds=10)
+    @tasks.loop(seconds=6)
     async def change_status(self):
-        await self.dictator.change_presence(activity=discord.Game(next(self.status)))
+        status = next(self.status)
+        activity = discord.Activity(name=status[1], type=status[0])
+        await self.dictator.change_presence(activity=activity)
 
 
 async def setup(dictator: commands.Bot) -> None:
