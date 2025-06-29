c3f1f421af554b32adf1468b97155938a78ae7c8
Connor Smith <connorhsmith@gmail.com>
2025-06-29 23:04:11 +1000

    Add LIMIT to specific queries.
    With our databse configuration, we must read all returned results, though in some cases we only expect one result and we don't want to overhaul use of the database for this case.

--- dictator/utils/utils.py
+++ dictator/utils/utils.py
@@ -4,12 +4,12 @@ from db_manager import db_connection as db_conn
 def get_user_by_discord_id(discord_id: int):
     with db_conn() as db:
         db.execute(
-            f"SELECT email, login_key FROM ticketServer_tickets WHERE discord_id = '{discord_id}'"
+            f"SELECT email, login_key FROM ticketServer_tickets WHERE discord_id = '{discord_id}' LIMIT 1"
         )
         return db.fetchone()
 
 
 def get_user_by_username(username: str):
     with db_conn() as db:
-        db.execute(f"SELECT email FROM ticketServer_tickets WHERE email = '{username}'")
+        db.execute(f"SELECT email FROM ticketServer_tickets WHERE email = '{username}' LIMIT 1")
         return db.fetchone()
